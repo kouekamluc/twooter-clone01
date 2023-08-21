5d868d4f615c91bce4d731feb4e583ca467ec55e
kouekamluc <luckevinkouekam@gmail.com>
2023-08-21 14:05:26 +0100

    fixing 5th commit

--- users/views.py
+++ users/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from sawo import createTemplate, getContext, verifyToken
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}.')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+
+class UserCreateView(SuccessMessageMixin , CreateView):
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_message = 'Your account was created successfully'
+    success_url = reverse_lazy('login')
 
 
 @login_required
